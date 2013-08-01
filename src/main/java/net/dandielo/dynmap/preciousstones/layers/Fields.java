edf3f2771ba0fcd2045ac89ca479c32357e72377
laseromich <0mi4@mail.ru>
2013-08-01 11:31:55 +0700

    Quick blocks with meta fix...

--- src/main/java/net/dandielo/dynmap/preciousstones/layers/Fields.java
+++ src/main/java/net/dandielo/dynmap/preciousstones/layers/Fields.java
@@ -199,7 +199,8 @@ public class Fields {
 	}
 
 	private String getFieldPopup(AreaMarker m, Field f) {
-		FieldStyle as = field_styles.get(f.getTypeId() + "_style");
+		int data =f.getBlock().getData();
+		FieldStyle as = field_styles.get(f.getTypeId()+((data==0)?"":new Integer(data).toString()) +"_style"); 
 		String v = "<div class=\"regioninfo\">" + formatAreaText(as.area_text_format)
 				+ "</div>";
 		return replaceSubstitutions(v, f);
@@ -246,7 +247,8 @@ public class Fields {
 
 	private void addStyle(AreaMarker m, Field f) {
 
-		FieldStyle as = field_styles.get(f.getTypeId() + "_style");
+		int data =f.getBlock().getData();
+		FieldStyle as = field_styles.get(f.getTypeId()+((data==0)?"":new Integer(data).toString()) +"_style");
 
 		if (as == null) {
 			as = default_style;
